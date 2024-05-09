e6919027177ea1a8add466524737d33fe389443a
nyutu_grace <nyutu_grace15@gmail.com>
2024-05-09 22:12:13 +0100

    chat completion openai

--- scripts/config.py
+++ scripts/config.py
@@ -7,7 +7,7 @@ x = load_dotenv(".env")
 
 class Config:
     RANDOM_SEED: int = int(os.getenv("RANDOM_SEED", default="1729"))
-    OPENAI_API_KEY: str = os.getenv("sk-proj-TxjgIUKpU0twgu9OvnIbT3BlbkFJ4RJ1lt3hCT5ZB9Mv7vYH")
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
 
     if OPENAI_API_KEY is None:
         raise EnvironmentError("Environment variables not found. Check your .env file or environment.")
